ee2918eb3b62f1a48c4ee973d1223d63cc4415c1
Your Name <you@example.com>
2022-07-14 14:29:24 -0300

    date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleServices.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleServices.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.entitys.Sale;
@@ -10,12 +14,16 @@ import com.devsuperior.dsmeta.repository.SalesRepository;
 
 @Service
 public class SaleServices {
-	
+
 	@Autowired
 	private SalesRepository repository;
-	
-	public List<Sale> findSales() {
-		return this.repository.findAll();
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		LocalDate min = minDate.equals("") ? today.minusYears(1) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		return this.repository.findSales(min, max, pageable);
 	}
 
 }
