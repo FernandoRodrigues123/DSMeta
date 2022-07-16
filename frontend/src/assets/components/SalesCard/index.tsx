3e5bb5ad7fee6ae26aac1ef81cfce65694bcf39e
Your Name <you@example.com>
2022-07-16 18:08:22 -0300

    Date update

--- frontend/src/assets/components/SalesCard/index.tsx
+++ frontend/src/assets/components/SalesCard/index.tsx
@@ -14,13 +14,20 @@ const max = new Date();
     const [minDate, setmiDate] = useState(min);
     const [maxDate, setMaxDate] = useState(max);
 
+
+
     const [sales, setSales] = useState<Sale[]>([])
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(response => {
+        const dmin = minDate.toISOString().slice(0,10);
+        const dmax = maxDate.toISOString().slice(0,10);
+
+        console.log(dmin)
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
            setSales(response.data.content)
         })
-    },[])
+    },[minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
